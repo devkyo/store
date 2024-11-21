c37d36d2e4d63d36a36b75974d6553144e0d0ff1
LucasAlvaresA <lucas.alvaress@hotmail.com>
2024-11-21 07:33:30 -0300

    feat: create header, footer and logo components

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import { Logo } from "@/components/logo";
+import { ThemeToggle } from "./theme-toggle";
+
+export const Header = () => {
+    return (
+        <header className="flex justify-between items-center my-5 mx-3">
+            <div className="flex items-center gap-3">
+                <Logo />
+            </div>
+            <div className="flex items-center gap-3">
+                <ThemeToggle />
+            </div>
+        </header>
+    );
+};
---
+++ src/components/logo.tsx
@@ -0,0 +1,7 @@
+export const Logo = () => {
+    return (
+        <div className="text-xl">
+            ShadcnUI <span className="font-bold">Store</span>
+        </div>
+    );
+};
